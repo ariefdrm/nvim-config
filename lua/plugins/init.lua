d8293498d67dcf6ab8fd48c3816fdb5562e5e314
ariefdrm <ariefdarmawan5522@gmail.com>
2023-04-13 06:39:21 +0700

    first commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,105 @@
+return {
+  	-- core plugins (penyedia library neovim lua)
+	{ "nvim-lua/plenary.nvim", event = "VeryLazy" },
+  -- color scheme
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("config.tokyonight")
+			require("config.colorscheme")
+		end,
+	},
+  -- programming
+	-- coloring
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "BufRead",
+		cmd = {
+			"TSBufDisable",
+			"TSBufEnable",
+			"TSBufToggle",
+			"TSDisable",
+			"TSEnable",
+			"TSToggle",
+			"TSInstall",
+			"TSInstallInfo",
+			"TSInstallSync",
+			"TSModuleInfo",
+			"TSUninstall",
+			"TSUpdate",
+			"TSUpdateSync",
+		},
+		build = ":TSUpdate",
+		-- build = function()
+		-- 	require("nvim-treesitter.install").update({ with_sync = true })()
+		-- end,
+		config = function()
+			require("config.treesitter")
+		end,
+	},
+	-- auto completion
+	{
+		"hrsh7th/nvim-cmp",
+		event = "BufWinEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			require("config.cmp")
+		end,
+	},
+  -- snippets
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+      -- stylua: ignore
+      keys = {
+        { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+        { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+      },
+	},
+	-- lsp
+	{
+		"neovim/nvim-lspconfig",
+		event = "BufWinEnter",
+		config = function()
+			require("config.lsp")
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = true,
+	},
+	{
+		"williamboman/mason.nvim",
+		event = "VeryLazy",
+		cmd = {
+			"Mason",
+			"MasonInstall",
+			"MasonUninstall",
+			"MasonUninstallAll",
+			"MasonLog",
+		},
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		init = function()
+			vim.tbl_map(function(plugin)
+				pcall(require, plugin)
+			end, { "lspconfig", "null-ls" })
+		end,
+	},
+  
+}
