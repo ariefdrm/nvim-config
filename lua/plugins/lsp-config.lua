51ebfecf246af24d698e7d8b6e57fceef8a05fbb
ariefdrm <ariefdarmawan5522@gmail.com>
2024-08-31 14:21:38 +0700

    menambahkan plugin untuk auto comment di neovim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,78 +1,82 @@
 return {
-  -- lsp server config
-  {
-    "williamboman/mason.nvim",          -- Mason nvim
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim", -- mason lsp config
-    },
-    lazy = lazy,
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "clangd",
-          "tsserver",
-          "html",
-          "omnisharp",
-          -- "trivy",
-          -- "stylua",
-          -- "clang_format",
-          -- "codelldb",
-          -- "cpplint",
-          -- "prettier",
-        },
-      })
-    end,
-  },
+	-- lsp server config
+	{
+		"williamboman/mason.nvim", -- Mason nvim
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim", -- mason lsp config
+		},
+		lazy = lazy,
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+			require("mason-lspconfig").setup({
+				-- ensure install language server
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"tsserver",
+					"html",
+					"omnisharp",
+				},
+			})
+		end,
+	},
 
-  -- Nvim lsp config
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	-- Nvim lsp config
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+			-- lua lspconfig
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.clangd.setup({
-        cmd = { "clangd" },
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-        root_pattern = {
-          ".clangd",
-          ".clang-tidy",
-          ".clang-format",
-          "compile_commands.json",
-          "compile_flags.txt",
-          "configure.ac",
-          ".git",
-        },
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-      })
+			-- c/c++ lspconfig
+			lspconfig.clangd.setup({
+				cmd = { "clangd" },
+				filetypes = { "c", "cpp", "objc", "objcpp" },
+				root_pattern = {
+					".clangd",
+					".clang-tidy",
+					".clang-format",
+					"compile_commands.json",
+					"compile_flags.txt",
+					"configure.ac",
+					".git",
+				},
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+			})
 
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
+			-- html lspconfig
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
+			-- javascript / typescript lspconfig
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
 
-      -- keymaps
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-    end,
-  },
+			-- C# lspconfig
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+			})
+
+			-- keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,18 @@
 return {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
 
-        config.setup({
-            auto_install = true,
-            sync_install = false,
-            -- highlighting 
-            highlight = {
-              enable = true,
-            },
-            -- Indent
-            indent = { enable = true },
-        })
-    end 
-
+		config.setup({
+			auto_install = true,
+			sync_install = false,
+			-- highlighting
+			highlight = {
+				enable = true,
+			},
+			-- Indent
+			indent = { enable = true },
+		})
+	end,
 }
-
-
-
