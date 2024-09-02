78e60cc77dd0e015c54a3b56856fd97ea426dac5
ariefdrm <ariefdarmawan5522@gmail.com>
2024-09-02 07:30:27 +0700

    update opt for tokyonight and toggleterm plugins

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,15 +1,15 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = function()
-    require("toggleterm").setup({})
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({})
 
-    -- Keybindings for better terminal usage
-    local opts = { noremap = true, silent = true }
-    vim.api.nvim_set_keymap("i", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", opts)
-    vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", opts)
+		-- Keybindings for better terminal usage
+		local opts = { noremap = true, silent = true }
+		-- vim.api.nvim_set_keymap("i", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", opts)
+		vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", opts)
 
-    -- Keybinding to close terminal with Esc
-    vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n><cmd>ToggleTerm<CR>", opts)
-  end,
+		-- Keybinding to close terminal with Esc
+		vim.keymap.set("t", "<Esc>", "<C-\\><C-n><cmd>ToggleTerm<CR>", opts)
+	end,
 }
